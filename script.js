eaa80639dca611a3a602b9cdf89e96a919822c9d
Iqrazaheerahmed <iqrazaheerhere@gmail.com>
2024-10-04 19:06:20 +0500

    files added

---
+++ script.js
@@ -0,0 +1,16 @@
+let circles= document.querySelectorAll(".circle"),
+light =0;
+
+setInterval(()=>{
+    changeLight();
+},1000);
+
+function  changeLight(){
+    circles[light].classList="circle";
+    light++;
+    if(light>2){
+        light=0;
+    }
+    const selectLight =circles[Light];
+    selectLight.classList.add(selectLight.getAttribute);
+}
